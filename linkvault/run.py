094199b804dca7d0009c53278f1c07de58ea21ba
Vrusha Naik <15vrusha@gmail.com>
2025-10-30 19:07:51 +0530

    added requirements and run

---
+++ linkvault/run.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+# Add project root to Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from app import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
